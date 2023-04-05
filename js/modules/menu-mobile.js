1cb89d5f889470a824722ab86bb922faf3c0c8e0
joaopedrojardim <joaopedrocjardim@gmail.com>
2023-04-05 10:58:09 -0300

    Ajustes no projeto

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -10,7 +10,7 @@ export default class MenuMobile {
     this.openMenu = this.openMenu.bind(this)
   }
 
-  openMenu() {
+  openMenu(event) {
     this.menuList.classList.add(this.activeClass)
     this.menuButton.classList.add(this.activeClass)
     outsideClick(this.menuList, this.events, () => {
